26d9d95c794daa161cd5390f2d1eb51be7219bf9
Likhith Varma M <likhith2001@gmail.com>
2021-04-24 10:24:24 +0530

    Update HappyNumber.c

--- HappyNumber.c
+++ HappyNumber.c
@@ -1,3 +1,21 @@
+/*C program to check if the given number is Happy Number or not !!
+A number is said to be happy if it yields 1 when replaced by the sum of squares of its digits repeatedly. 
+If this process results in an endless cycle of numbers containing 4, then the number will be an unhappy number.
+Example 
+Number = 32
+32+ 22 = 13
+12 + 32 = 10
+12 + 02 = 1   Happy Number 
+
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+82 is a happy number number
+
+Some of the other examples of happy numbers are 7, 28, 100, 320 and so on.
+
+The unhappy number will result in a cycle of 4, 16, 37, 58, 89, 145, 42, 20, 4, ..*/
+
 #include<stdio.h>
 
 int squares_sum(int n){
